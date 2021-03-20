df4f23c59f38a0dbe4d4cef4537fc939e1fe139a
Aditya <37655818+adi-075@users.noreply.github.com>
2021-03-20 12:43:39 +0530

    Add Memory Location Programs
    Added 'Two 8 bit nos. stored at Memory Location'

---
+++ Memory Location Programs/mlsub.asm
@@ -0,0 +1,18 @@
+
+;<Add two 8 bit number stored at Memory Location 2050H and 2051h and store the result in 2053H>
+
+jmp start
+
+;12+13=25
+
+
+;code
+start: nop
+
+LDA 2050H
+MOV B, A
+LDA 2051H
+SUB B
+STA 2053H
+
+hlt
